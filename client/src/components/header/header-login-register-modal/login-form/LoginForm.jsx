ee842cd0b3ed1107a804d0f5643d5e6e7780acad
Ivan <ivan.st3fan0v@gmail.com>
2024-07-27 12:28:00 +0300

    Add react-hook-form

--- client/src/components/header/header-login-register-modal/login-form/LoginForm.jsx
+++ client/src/components/header/header-login-register-modal/login-form/LoginForm.jsx
@@ -1,4 +1,5 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { login } from '../../../../services/authService';
 import { setUserData } from '../../../../utils/utils';
@@ -7,61 +8,98 @@ import UserContext from '../../../../contexts/UserContext';
 export default function LoginForm({
     hideLoginRegisterModal
 }) {
+    const {
+        register,
+        handleSubmit,
+        setError,
+        clearErrors,
+        formState: { errors, isSubmitting }
+    } = useForm({
+        defaultValues: {
+            'email': '',
+            'password': ''
+        }
+    });
     const UserCTX = useContext(UserContext)
 
-    const [submitError, setSubmitError] = useState('');
-    const [formValues, setFormValues] = useState({});
+    async function loginSubmitHandler(data) {
 
-    function changeHandler(e) {
-        setFormValues(oldValues => ({
-            ...oldValues,
-            [e.target.name]: e.target.value
-        }))
-    }
-
-    async function loginSubmitHandler(event) {
-        event.preventDefault();
-
+        const { email, password } = data;
         try {
-            const userData = await login(formValues.email, formValues.password);
+            const userData = await login(email, password);
+
             setUserData(userData);
             hideLoginRegisterModal();
             UserCTX.updateUser(userData);
             navigate('/');
         } catch (error) {
-            setSubmitError(error.message);
+            setError('serverError', {
+                type: 'serverErrMsg',
+                message: error.message == 'Failed to fetch' ? 'Please try again later, there are some technical issues' : error.message
+            });
+
+        }
+    }
+
+    function onError(errors) {
+        if(errors.serverError) {
+            clearErrors('serverError');
+            handleSubmit(loginSubmitHandler)();
         }
     }
 
     return (
         <div className="form">
-            <form onSubmit={loginSubmitHandler}>
+            <form onSubmit={handleSubmit(loginSubmitHandler, onError)}>
                 <div className="form__head">
                     <h5>Login</h5>
                 </div>
 
                 <div className="form__body">
-                    {submitError &&
+                    {errors.serverError &&
                         <div className="form__error">
-                            <p>{submitError}</p>
+                            <p>{errors.serverError.message}</p>
                         </div>
                     }
 
-                    <div className="form__row">
+                    <div className={errors.email ? 'form__row form__row--err' : 'form__row'}>
                         <label hmtlfor="email"
                             className="form__label">Email</label>
 
                         <div className="form__controls">
-                            <input type="email" className="field" id="email" name="email" autoComplete="off" onChange={changeHandler}></input>
+                            <input
+                                type="email"
+                                className="field"
+                                id="email"
+                                name="email"
+                                autoComplete="off"
+                                {...register('email', {
+                                    required: 'This field is required.'
+                                })}
+                            ></input>
+                            {errors.email &&
+                                <p className='err-msg'>{errors.email.message}</p>
+                            }
                         </div>
                     </div>
 
-                    <div className="form__row">
+                    <div className={errors.password ? 'form__row form__row--err' : 'form__row'}>
                         <label hmtlfor="password"
                             className="form__label">Password</label>
 
                         <div className="form__controls">
-                            <input type="password" className="field" id="password" name="password" onChange={changeHandler}></input>
+                            <input
+                                type="password"
+                                className="field"
+                                id="password"
+                                name="password"
+                                {...register('password', {
+                                    required: 'This field is required'
+                                })}
+                            ></input>
+                            {errors.password &&
+                                <p className='err-msg'>{errors.password.message}</p>
+                            }
                         </div>
                     </div>
                 </div>
