c95a450b7c748795a3cb51a824f5ce9b18e55a23
Ivan <ivan.st3fan0v@gmail.com>
2024-08-07 09:55:50 +0300

    Change link to button for user modal toggler

--- client/src/components/header/header-login-utils/HeaderLoginUtils.jsx
+++ client/src/components/header/header-login-utils/HeaderLoginUtils.jsx
@@ -1,11 +1,9 @@
-import { Link } from "react-router-dom";
-
 export default function HeaderLoginUtils({showLoginRegisterMmodal}) {
 	return (
 		<div className="header__utils">
-			<Link href="#" className="login ico-background" onClick={showLoginRegisterMmodal}>
+			<button  className="login ico-background" onClick={showLoginRegisterMmodal}>
 				<img src="../src/assets/svgs/user-ico.svg" alt=""></img>
-			</Link>
+			</button>
 		</div>
 	);
 }
--- client/src/components/header/header-user-utils/HeaderUserUtils.jsx
+++ client/src/components/header/header-user-utils/HeaderUserUtils.jsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { UserContext } from "../../../contexts/UserContext";
 
 export default function HeaderUserUtils({
@@ -9,9 +8,9 @@ export default function HeaderUserUtils({
 
 	return (
 		<div className="header__user-utils">
-			<Link href="#" className="user-utils ico-background" onClick={showUserInfo}>
+			<button href="#" className="user-utils ico-background" onClick={showUserInfo}>
 				<img src={UserCTX.user.imageUrl} alt="" />
-			</Link>
+			</button>
 		</div>
 	);
 }
